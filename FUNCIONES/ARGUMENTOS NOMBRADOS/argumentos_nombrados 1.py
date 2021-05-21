7120d6d2fea630b2af83f5fd29098e45d0e95dd8
Kelly Gutierrez <78502119+KellyGutierrez@users.noreply.github.com>
2021-05-20 19:26:38 -0500

    Add files via upload

---
+++ FUNCIONES/ARGUMENTOS NOMBRADOS/argumentos_nombrados 1.py
@@ -0,0 +1,12 @@
+def calcular_sueldo(nombre,costohora,cantidadhoras):
+    sueldo=costohora*cantidadhoras
+    print(nombre,"trabajo",cantidadhoras,"y cobra un sueldo de",sueldo)
+
+
+# bloque principal
+
+calcular_sueldo("samuel",10,120)
+calcular_sueldo(costohora=12,cantidadhoras=40,nombre="daniela")
+calcular_sueldo(cantidadhoras=90,nombre="camilo",costohora=7)
+
+
